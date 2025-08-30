e9e12ab803a9a7757b6f95790043c670cd63a296
Alok <alokskj14@gmail.com>
2025-08-30 13:59:39 +0530

    feat: Setup db schema

--- client/src/app/router/routes/user/index.ts
+++ client/src/app/router/routes/user/index.ts
@@ -2,40 +2,40 @@ import React, { lazy } from "react";
 import { RouteObject } from "react-router";
 import { CitizenRoute } from "@/modules/auth";
 
-// Lazy load auth pages
-const ReportsPage = lazy(() => 
-  import('@/modules/reports').then(module => ({ default: module.ReportsListPage }))
-);
+// // Lazy load auth pages
+// const ReportsPage = lazy(() => 
+//   import('@/modules/reports').then(module => ({ default: module.ReportsListPage }))
+// );
 
-const CreateReportPage = lazy(() => 
-  import('@/modules/reports').then(module => ({ default: module.CreateReportPage }))
-);
+// const CreateReportPage = lazy(() => 
+//   import('@/modules/reports').then(module => ({ default: module.CreateReportPage }))
+// );
 
-const ReportDetailsPage = lazy(() => 
-  import('@/modules/reports').then(module => ({ default: module.ReportDetailsPage }))
-);
+// const ReportDetailsPage = lazy(() => 
+//   import('@/modules/reports').then(module => ({ default: module.ReportDetailsPage }))
+// );
 
-const MyReportsPage = lazy(() => 
-  import('@/modules/reports').then(module => ({ default: module.MyReportsPage }))
-);
+// const MyReportsPage = lazy(() => 
+//   import('@/modules/reports').then(module => ({ default: module.MyReportsPage }))
+// );
 
 const userRoutes: RouteObject[] = [
-    { 
-        path: '/', 
-        element: React.createElement(CitizenRoute, null, React.createElement(ReportsPage))
-    },
-    { 
-        path: '/reports/create', 
-        element: React.createElement(CitizenRoute, null, React.createElement(CreateReportPage))
-    },
-    { 
-        path: '/reports/:id', 
-        element: React.createElement(CitizenRoute, null, React.createElement(ReportDetailsPage))
-    },
-    { 
-        path: '/my-reports', 
-        element: React.createElement(CitizenRoute, null, React.createElement(MyReportsPage))
-    },
+    // { 
+    //     path: '/', 
+    //     element: React.createElement(CitizenRoute, null, React.createElement(ReportsPage))
+    // },
+    // { 
+    //     path: '/reports/create', 
+    //     element: React.createElement(CitizenRoute, null, React.createElement(CreateReportPage))
+    // },
+    // { 
+    //     path: '/reports/:id', 
+    //     element: React.createElement(CitizenRoute, null, React.createElement(ReportDetailsPage))
+    // },
+    // { 
+    //     path: '/my-reports', 
+    //     element: React.createElement(CitizenRoute, null, React.createElement(MyReportsPage))
+    // },
 ];
 
 export default userRoutes;
